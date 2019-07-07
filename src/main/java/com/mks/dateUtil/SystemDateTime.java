09df4be9cfc4e2b036536f8c43b5deddbb1f183c
Manju-crz <manju.crz@gmail.com>
2019-07-07 12:33:45 +0530

    Base defination 1.7

---
+++ src/main/java/com/mks/dateUtil/SystemDateTime.java
@@ -0,0 +1,19 @@
+package com.mks.dateUtil;
+
+public class SystemDateTime {
+	
+	
+	public void getCurrentSystemDate() {
+		
+	}
+	
+
+	public void getCurrentSystemDate(int withDateDiff) {
+		
+	}
+	
+	
+	
+	
+	
+}
